fffc3171b933696fb3263dbbeb1982fc678df674
CHUDAI0296 <1504478674@qq.com>
2025-06-17 23:32:10 +0800

    feat: 添加 Google 站点验证 meta 标签

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WERZ70HF93"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WERZ70HF93');
+</script>
+      </head>
       <body>{children}</body>
     </html>
   )
